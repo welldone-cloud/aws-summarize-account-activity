0380c8bc16f70299eb222ccdda05acc2d8f7ac00
michael-kirchner-at <michael.kirchner@welldone.cloud>
2023-05-30 08:49:27 +0200

    Added expected file name pattern in error output

--- generate_plots_for_existing_json_file.py
+++ generate_plots_for_existing_json_file.py
@@ -7,7 +7,7 @@ import sys
 from aws_summarize_account_activity import generate_plot_files
 
 
-EXPECTED_FILE_FORMAT_REGEX = "^account_activity_(\\d+)_(\\d+).json$"
+EXPECTED_FILE_FORMAT_REGEX = "account_activity_(\\d+)_(\\d+).json"
 
 
 if __name__ == "__main__":
@@ -28,19 +28,24 @@ if __name__ == "__main__":
     args = parser.parse_args()
     file_name = args.file[0]
 
-    # Read source file details
+    # Read source file
     file_name_without_path = os.path.basename(file_name.name)
-    captures = re.search(EXPECTED_FILE_FORMAT_REGEX, file_name_without_path)
+    captures = re.fullmatch(EXPECTED_FILE_FORMAT_REGEX, file_name_without_path)
     if captures:
         account_id = captures.group(1)
         run_timestamp = captures.group(2)
     else:
-        print("Unexpected file name pattern received: {}".format(file_name_without_path))
+        print("Unexpected file name received: {}".format(file_name_without_path))
+        print("Expected pattern: {}".format(EXPECTED_FILE_FORMAT_REGEX))
+        sys.exit(1)
+    try:
+        result_collection = json.load(file_name)
+    except json.decoder.JSONDecodeError:
+        print("Invalid JSON content")
         sys.exit(1)
 
     # Write plot files
     print("Generating plots")
-    result_collection = json.load(file_name)
     output_dir_name = "account_activity_{}_{}_plots".format(account_id, run_timestamp)
     generate_plot_files(result_collection, output_dir_name)
     print("Plot files written to {}".format(output_dir_name))
